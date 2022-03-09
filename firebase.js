84561fa41dc722e6a000ddf10b60746b0f88b4c4
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-03-09 19:16:27 +0530

    chores: added firebase config file

--- firebase.js
+++ firebase.js
@@ -3,7 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    // Config here
+    apiKey: "AIzaSyDQf1SMg6JTP7Mkfco1V4JIbsJXN6vhJkI",
+    authDomain: "noir-7afa3.firebaseapp.com",
+    projectId: "noir-7afa3",
+    storageBucket: "noir-7afa3.appspot.com",
+    messagingSenderId: "686202027293",
+    appId: "1:686202027293:web:0f0a90094b92d32808f5f4",
+    measurementId: "G-XBBPG4GK1Z"
 };
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
