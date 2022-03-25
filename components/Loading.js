6b8de2e8e4479729f6ae75c7a7786f50e9b8f868
Gourav <grvrawat999@gmail.com>
2022-03-25 20:41:51 +0530

    refactoring to latest @mui

--- components/Loading.js
+++ components/Loading.js
@@ -1,21 +1,29 @@
 import styled from "styled-components";
-import { Wave } from 'better-react-spinkit';
+import { Wave } from "better-react-spinkit";
+import Image from "next/image";
 
 const Center = styled.div``;
 
 const Loading = () => {
-    return (
-        <Center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/1257px-Ethereum_logo_2014.svg.png"
-                    alt=""
-                    height={100}
-                    style={{ marginBottom: 10 }}
-                ></img>
-                <Wave size={40}></Wave>
-            </div>
-        </Center>
-    )
+  return (
+    <Center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/1257px-Ethereum_logo_2014.svg.png"
+          alt=""
+          height={100}
+          style={{ marginBottom: 10 }}
+        ></Image>
+        <Wave size={40}></Wave>
+      </div>
+    </Center>
+  );
 };
 
 export default Loading;
