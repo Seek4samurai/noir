85850041db13cd328f225e6df34d293fd0c58cdf
Gourav <grvrawat999@gmail.com>
2022-04-16 15:49:26 +0530

    chore: ssr chat to client side

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,10 +1,12 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
 import ChatScreen from "../../components/ChatScreen";
 import Sidebar from "../../components/Sidebar";
 import getEmail from "../../utils/getEmail";
 import { auth, db } from "../../firebase";
+import { useEffect, useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -20,52 +22,57 @@ const ChatContainer = styled.div`
   scrollbar-width: none;
 `;
 
-const Chat = ({ chat, messages }) => {
+const Chat = () => {
+  const [chat, setChat] = useState();
+  const [messages, setMessages] = useState();
+  const [userName, setUserName] = useState();
   const [user] = useAuthState(auth);
-  const userName = getEmail(chat.users, user).split("@");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.id) {
+      const fetchData = async () => {
+        const ref = db.collection("chats").doc(router.query.id);
+        const messagesRes = await ref
+          .collection("messages")
+          .orderBy("timestamp", "asc")
+          .get();
+
+        const messages = messagesRes.docs
+          .map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+          .map((messages) => ({
+            ...messages,
+            timestamp: messages.timestamp.toDate().getTime(),
+          }));
+
+        const chatRes = await ref.get();
+        const chat = {
+          id: chatRes.id,
+          ...chatRes.data(),
+        };
+
+        setChat(chat);
+        setMessages(JSON.stringify(messages));
+        setUserName(getEmail(chat.users, user).split("@"));
+      };
+
+      fetchData();
+    }
+  }, [router.query.id]);
 
   return (
     <Container>
-      <Head>
-        <title>Chat with {userName[0]}</title>
-      </Head>
       <Sidebar></Sidebar>
       <ChatContainer>
-        <ChatScreen chat={chat} messages={messages}></ChatScreen>
+        {chat && messages ? (
+          <ChatScreen chat={chat} messages={messages}></ChatScreen>
+        ) : null}
       </ChatContainer>
     </Container>
   );
 };
 
 export default Chat;
-
-export async function getServerSideProps(context) {
-  const ref = db.collection("chats").doc(context.query.id);
-  const messagesRes = await ref
-    .collection("messages")
-    .orderBy("timestamp", "asc")
-    .get();
-
-  const messages = messagesRes.docs
-    .map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }))
-    .map((messages) => ({
-      ...messages,
-      timestamp: messages.timestamp.toDate().getTime(),
-    }));
-
-  const chatRes = await ref.get();
-  const chat = {
-    id: chatRes.id,
-    ...chatRes.data(),
-  };
-
-  return {
-    props: {
-      messages: JSON.stringify(messages),
-      chat: chat,
-    },
-  };
-}
